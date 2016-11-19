5662a361c8efd93554202ae2a07020b63fd9e7c3
Nikhil Arora <narora200@gmail.com>
2016-11-19 09:28:33 +0530

    Export to Excel button added

--- resources/views/export.blade.php
+++ resources/views/export.blade.php
@@ -12,7 +12,7 @@
 <p id="profile-name" class="profile-name-card" style="font-size:2em;color:#000;font-family: 'Lato', sans-serif;">Export Your Data</p>
             <div>&nbsp;</div>
 <div class="text-center">
-<a href="{{ route('admin.info.excel') }}">Export to Excel</a>
+<a href="{{ route('admin.info.excel') }}" class="btn btn-success">Export to Excel</a>
 </div>
 </div>
 </div>
