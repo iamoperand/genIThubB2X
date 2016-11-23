9988daafb1395be9ea10d6eaf2f783a3a5f01670
Nikhil Arora <narora200@gmail.com>
2016-11-23 15:57:19 +0530

    Calendar redesigned

--- resources/views/export.blade.php
+++ resources/views/export.blade.php
@@ -12,10 +12,10 @@
 <p id="profile-name" class="profile-name-card" style="font-size:2em;color:#000;font-family: 'Lato', sans-serif;">Export Your Data</p>
             <div>&nbsp;</div>
              <form method="post" action="{{route('admin.info.excel')}}">
-     <div class="input-group input-daterange" data-provide="datepicker" data-date-format="yyyy-mm-dd">
-    <input type="text" class="form-control" name="start" placeholder="Start Date">
+     <div class="input-group input-daterange">
+    <input type="text" class="form-control datepicker_start" name="start" placeholder="Start Date">
     <span class="input-group-addon">to</span>
-    <input type="text" class="form-control" name="finish" placeholder="End Date">
+    <input type="text" class="form-control datepicker_finish" name="finish" placeholder="End Date">
 
 </div>
 
@@ -33,7 +33,26 @@
 </div>
 
 <script>
-	
+	$('.datepicker_start').datepicker({
+    
+    format: 'yyyy-mm-dd',
+    autoclose: true,
+    clearBtn: true,
+    daysOfWeekDisabled: '0',
+    title: "Choose initial date",
+    todayBtn: "linked",
+    todayHighlight: true,
+});
+	$('.datepicker_finish').datepicker({
+    
+    format: 'yyyy-mm-dd',
+    autoclose: true,
+    clearBtn: true,
+    daysOfWeekDisabled: '0',
+    title: "Choose final date",
+    todayBtn: "linked",
+    todayHighlight: true,
+});
 </script>
 
 
